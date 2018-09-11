eaf945afc76fa60c5fe1971b5d9b4640bea9ff2c
Keyboard-cai <lisaboyss1@qq.com>
2018-09-11 19:34:41 +0800

    修改了afcun.add.handle.php的逻辑关系

--- admin/ACFUN.add.handle.php
+++ admin/ACFUN.add.handle.php
@@ -1,10 +1,5 @@
 <?php
     require_once('../connect.php');
-    if (!isset($_POST["name"]) || empty($_POST["name"])
-        || !isset($_POST["url"]) || empty($_POST["url"])
-        || !isset($_POST["parent_id"]) || empty($_POST["parent_id"])) {
-        echo '{"success":false,"msg":"参数错误，信息填写不全"}';
-    }
     $name = $_POST["name"];
     $url = 'http://' .$_POST["url"].'/';
     $sql = "select * from nav ORDER BY 'id'";
@@ -18,8 +13,14 @@
     }
     $insertsql = "insert into nav(name,url,parent_id) values('$name', '$url', '$parent_id')";
 
-    if(mysqli_query($con,$insertsql)){
-        echo '{"success":true,"msg":"信息保存成功!<br>菜单名称:'. $name .'<br>链接地址:' .  $url . '<br>父菜单:' .  $parent_id . '<br>"}';
+    if (!isset($_POST["name"]) || empty($_POST["name"])
+        || !isset($_POST["url"]) || empty($_POST["url"])) {
+        echo '{"success":false,"msg":"参数错误，信息填写不全"}';
+    }else{
+        if(mysqli_query($con,$insertsql)){
+            echo '{"success":true,"msg":"信息保存成功!<br>菜单名称:'. $name .'<br>链接地址:' .  $url . '<br>父菜单:' .  $parent_id . '<br>"}';
+        }
     }
+
 ?>
 
